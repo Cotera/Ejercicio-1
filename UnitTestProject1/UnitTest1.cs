63eb88304479656a3f7d4e3268219d2a286acdf8
usuario <usuario@L-1702033>
2017-07-06 18:03:18 +0200

    Subir Test Definitivo

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -22,30 +22,6 @@ namespace UnitTestProject1
 			string message = sut.composeText();
 			Assert.AreEqual("Mis numeros 1, 2.", message);
 		}
-
-		/*
-		 [TestInitialize]  // Atributo para ejecutar antes de los métodos de test.
-        public void TestInicial()
-        {
-            sut = new Class1();
-        }
-
-        [TestMethod] // Atributo (anotación Java) que indica que es un método de prueba, es la forma de que el código sepa que es tal, y no un código normal.
-        public void TestResta()
-        {
-            int res = sut.Resta(5, 2);
-
-            Assert.AreEqual(3, res);
-        }
-
-        [TestMethod]
-        public void TestRestaNegativa()
-        {
-            int res = sut.Resta(-5,-2);
-
-            Assert.AreEqual(-3, res);
-        }
-		 
-		 */
 	}
+
 }
