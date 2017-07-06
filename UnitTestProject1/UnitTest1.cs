dc6b8033d6d57358a2cf85b4ab2319f9a4faab92
usuario <usuario@L-1702033>
2017-07-06 17:59:18 +0200

    Subida definitiva con el ejercicio y test acabado.

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,15 +1,51 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ejercicio1;
 
 namespace UnitTestProject1
 {
 	[TestClass]
 	public class UnitTest1
 	{
+		private OperationClass sut;
+
+		[TestInitialize]  // Atributo para ejecutar antes de los métodos de test.
+		public void TestInicial()
+		{
+			sut = new OperationClass();
+			sut.initialice(1, 2, "Mis numeros");
+		}
+
 		[TestMethod]
 		public void TestMethod1()
 		{
-
+			string message = sut.composeText();
+			Assert.AreEqual("Mis numeros 1, 2.", message);
 		}
+
+		/*
+		 [TestInitialize]  // Atributo para ejecutar antes de los métodos de test.
+        public void TestInicial()
+        {
+            sut = new Class1();
+        }
+
+        [TestMethod] // Atributo (anotación Java) que indica que es un método de prueba, es la forma de que el código sepa que es tal, y no un código normal.
+        public void TestResta()
+        {
+            int res = sut.Resta(5, 2);
+
+            Assert.AreEqual(3, res);
+        }
+
+        [TestMethod]
+        public void TestRestaNegativa()
+        {
+            int res = sut.Resta(-5,-2);
+
+            Assert.AreEqual(-3, res);
+        }
+		 
+		 */
 	}
 }
