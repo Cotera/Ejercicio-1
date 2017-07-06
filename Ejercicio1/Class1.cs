eed650b7b1f804b1d9675a2d7c527b26a408ec58
usuario <usuario@L-1702033>
2017-07-06 17:51:04 +0200

    Subida inicial sin test.

--- Ejercicio1/Class1.cs
+++ Ejercicio1/Class1.cs
@@ -6,7 +6,61 @@ using System.Threading.Tasks;
 
 namespace Ejercicio1
 {
-    public class Class1
+    public class DataClass
     {
-    }
+		private int n1;
+
+		private int n2;
+
+		private string text;
+
+		public void SetN1 (int valor)
+		{
+			this.n1 = valor;
+		}
+
+		public void SetN2(int valor)
+		{
+			this.n2 = valor;
+		}
+
+		public void SetText(string valor)
+		{
+			this.text = valor;
+		}
+
+		public int GetN1()
+		{
+			return this.n1;
+		}
+
+		public int GetN2()
+		{
+			return this.n2;
+		}
+
+		public string GetText()
+		{
+			return this.text;
+		}
+	}
+
+
+	public class OperationClass
+	{
+		private DataClass datos;
+
+		public void initialice (int n1, int n2, string text)
+		{
+			datos = new DataClass();
+			datos.SetN1(n1);
+			datos.SetN2(n2);
+			datos.SetText(text);
+		}
+
+		public string composeText()
+		{
+			return datos.GetText() + " " + datos.GetN1() + ", " + datos.GetN2() + ".";
+		}
+	}
 }
